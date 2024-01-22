e4e9cb4d3b43c2dfcec87ee7fe34e1f7d27e90a0
Oto <92301254+WERSIT35@users.noreply.github.com>
2024-01-22 15:26:02 +0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    template: `
+    <router-outlet></router-outlet>
+
+    `,
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, HomeComponent, RouterModule]
 })
 export class AppComponent {
   title = 'ngular';
